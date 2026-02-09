956ce58113c8b3c6027ff914dc18f919937fe1c9
Erika <2530460+xne@users.noreply.github.com>
2026-02-08 21:09:28 -0500

    Add main menu scene

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button settingsButton;
+    [SerializeField] private Button quitButton;
+
+    [SerializeField] private GameObject settingsMenu;
+    [SerializeField] private Button backButton;
+
+    private readonly GameObjectStack stack = new();
+
+    private void Start()
+    {
+        stack.Push(mainMenu);
+    }
+
+    private void OnEnable()
+    {
+        playButton.onClick.AddListener(Play);
+        settingsButton.onClick.AddListener(Settings);
+        quitButton.onClick.AddListener(Quit);
+
+        backButton.onClick.AddListener(Back);
+    }
+
+    private void OnDisable()
+    {
+        playButton.onClick.RemoveAllListeners();
+        settingsButton.onClick.RemoveAllListeners();
+        quitButton.onClick.RemoveAllListeners();
+
+        backButton.onClick.RemoveAllListeners();
+    }
+
+    public void Play()
+    {
+    }
+
+    public void Settings()
+    {
+        stack.Push(settingsMenu);
+    }
+
+    public void Quit()
+    {
+        Game.Quit();
+    }
+
+    public void Back()
+    {
+        stack.Pop();
+    }
+}
