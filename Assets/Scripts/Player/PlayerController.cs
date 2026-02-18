e323ac2c8246b01c0bc84ae672cb2eed75e5ec7f
Erika <2530460+xne@users.noreply.github.com>
2026-02-18 00:57:34 -0500

    Add Cinemachine camera

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,27 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private CinemachineBrain cinemachineBrain;
+    [SerializeField] private CinemachineCamera cinemachineCamera;
+    [SerializeField] private CinemachineInputAxisController inputAxisController;
+
     private PlayerInput playerInput;
+
+    private OutputChannels cinemachineChannel;
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
+        inputAxisController.PlayerIndex = playerInput.playerIndex;
+
+        var firstChannel = (int)OutputChannels.Channel01;
+        cinemachineChannel = (OutputChannels)(firstChannel << playerInput.playerIndex);
+
+        cinemachineBrain.ChannelMask = cinemachineChannel;
+        cinemachineCamera.OutputChannel = cinemachineChannel;
     }
 }
